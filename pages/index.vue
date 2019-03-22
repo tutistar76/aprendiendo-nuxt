342c2ae9a9031b3bd3eafa3a68ac1c3f756898ef
tutistar <tuti.star25@gmail.com>
2019-03-21 20:41:47 -0500

    iniciando el proyecto

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>hola {{ msg }}</h1>
+    <!--//todo lo que se escibe aquie es html-->
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+      <tr>
+        <th>Nombre</th>
+      </tr>
+      <tr v-for="(item, index) in municipio" :key="index">
+        <!--para recorrer arreglos-->
+        <td>{{item}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "tutistar",
+      municipio: ["Cali", "popayan", "Mocoa"]
+    };
+  },
+  methods: {
+    guardar() {
+      this.municipio.push(this.msg);
+      this.msg = "";
+    }
+  }
+};
+</script>
+<style>
+</style>
